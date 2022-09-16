448712d12cba36e97bbc611ac4ae4fc6862f603c
rightdirection1 <lyubima.takova@gmail.com>
2022-09-16 11:25:24 +0300

    Added assets and the UI of CalorieCalculator.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import CalorieCalculator from './components/CalorieCalculator';
 
 function App() {
   return (
     <div className="App">
-    Daily Calories Calculator
+    <CalorieCalculator/>
     </div>
   );
 }
